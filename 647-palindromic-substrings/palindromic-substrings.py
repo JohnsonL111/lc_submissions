2221bccab0d943b4a2d522558811f4d1de618045
Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>
2025-10-23 13:29:05 -0700

    Time: 187 ms (32.61%) | Memory: 25.5 MB (16.89%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,26 +1,32 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # expand from center and go outwards 1 char at a time 
-        start = 0
-        last = len(s)
+        # i and j inclusive are start and end of substring
+        n = len(s)
+        dp = []
         count = 0
+        # init dp of all False
+        for i in range(n):
+            dp.append([False] * n)
+        rows = len(dp)
+        cols = len(dp[0])
+        print(f'max indexable is {rows - 1} and {cols - 1}')
 
-        for center in range(last):
-            # odd (center is char)
-            count += self.countPalindrome(s, center, center)
 
-            # even (center is between char)
-            count += self.countPalindrome(s, center, center+1)
+        # dp[i][j] = dp[i+1][j-1] and s[i] == s[j]
+        for i in range(n-1, -1, -1):
+            # in first iteration its trivially s[-1] to s[-1]
+            for j in range(i, n):
+                # print(f'i {i} j {j}')
+                # j-i <= 2 means no inner substring exist so skip dp lookup
+                if s[i] == s[j] and  (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    count += 1
+        
         return count
 
-    def countPalindrome(self, s, start, end) -> count:
-        count = 0
-        last = len(s)-1
-        while (start >= 0 and end <= last and s[start] == s[end]):
-            count += 1
-            start -= 1
-            end +=1
-        return count
+                
+        
+
 
 
 
