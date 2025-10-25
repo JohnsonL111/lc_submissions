6553a6e66c4d26ac036a08b7158e058793a8e48f
Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>
2025-10-25 16:06:09 -0700

    Time: 774 ms (51.71%) | Memory: 18.2 MB (57.53%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -9,10 +9,11 @@ class Solution:
         for i in range(1, len(dp)):
             # for each coin
             for c in coins:
-                # can use the coin
+                # can use the coin (just need 1 more coin then)
                 if i - c >= 0:
                     # initially dp[i] is inf
                     # +1 to add the coin
+                    # we take min becausse if we have multiple denominations we might find a denomination that better equates to dp[i]
                     dp[i] = min(dp[i], dp[i-c] + 1)
 
 
