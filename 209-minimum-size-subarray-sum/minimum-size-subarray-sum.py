48acdc375ff47087aa81f9397170e5a5ffda61dc
Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>
2025-12-29 14:51:14 -0800

    Time: 12 ms (29.70%) | Memory: 27.8 MB (92.91%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,15 +1,22 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left = 0
-        curr = 0
-        ans = float('inf')
+        # we need keep a variable that tracks the min length subarray whose sum is >= target
+        # sliding window approach: we need right and left pointers
+        # since we only have positive integers we know that if we add a value on the right it will always increase the size of the number
+        # we could keep sliding the right pointer until its >= target
+        # at that point we track its legnth using r-l+1 and check if its the smallest seen so far
+        # and then we slide the left pointer until >= target breaks and check the length r-l+1 against the current tracked min length seen so far
 
-        for right, x in enumerate(nums):
-            curr += x
-            # shrink while valid to get the minimal window ending at `right`
-            while curr >= target:
-                ans = min(ans, right - left + 1)
-                curr -= nums[left]
-                left += 1
+        left, right = 0, 0
+        end = len(nums)-1
+        minLen = float("inf")
+        currSum = 0
+        while right <= end:
+            currSum += nums[right]
+            while currSum >= target:
+                minLen = min(minLen, right-left+1)
+                currSum -= nums[left]
+                left+= 1
+            right+= 1
 
-        return 0 if ans == float('inf') else ans
+        return minLen if minLen != float("inf") else 0
