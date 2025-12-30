3e685fb34db6fb70e97c003fae58ee1b54bd847c
Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>
2025-12-30 13:08:35 -0800

    Time: 15 ms (80.89%) | Memory: 17.3 MB (90.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,21 +1,31 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        r = 0
-        l = 0
-        longest = 0
-        curr = set() # keeps track of current longest substring
+        # without duplicate characters suggests we can use a set to hold our current substring
+        # a set gives us o(1) access (hashset)
+        # we want to maintain a window between L and R pointers and keep track of its length as it grows
+        # we want to update a longest substring length variable as we grow the window
+        # when there is a duplicate character at R we want to shrink from the left and remove the left character from the set until there are no duplicates
+        # when our R pointer reaches the end of the string we can return the longest substrength length variable found
 
-        n = len(s)
-        for r in range(n):
-            c = s[r]
-            while c in curr:
-                curr.remove(s[l])
-                l += 1
-            # add it
-            curr.add(s[r])
-            # r += 1 # this auto gets incremented
+        l, r = 0, 0
+        substring = set()
+        longest = 0
+        end = len(s)-1
+
+        while r <= end:
+            char = s[r]
+
+            # char exists already
+            while char in substring:
+                char_remove = s[l]
+                substring.remove(s[l])
+                l+=1
             
-            longest = max(longest, r-l+1)
+            # no dupes at this point onwards
+            substring.add(char)
+            longest = max(len(substring), longest)
+            r+=1
+
         return longest
 
 
