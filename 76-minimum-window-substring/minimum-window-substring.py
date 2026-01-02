c9c5a9aabefc9f64bddca58df9bb8a3cc4d4cd9b
Johnson Luong <38903775+JohnsonL111@users.noreply.github.com>
2026-01-01 22:46:00 -0800

    Time: 662 ms (6.05%) | Memory: 17.9 MB (90.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,28 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        l = 0
-        n = len(s)
-        r = 0
-        bestMatch = ""
-        countS = Counter()
-        countT = Counter(t)
+        # if the size of s > size of t then return empty string
+        # we need a right and left pointer for creating a window between L and R
+        # create a static Counter hashmap of t 
+        # update a hashmap of the window as we parse through s
+        # at every valid window (valid window is when the length of the window >= length of t)
+        # constantly check the length of windows that are the same as t (track the minimum length substring found)
+        # when the window is invalid and not a substring match, fix it by moving the left pointer until at least the size of the window >= length of t
+        # when the right pointer goes past the last index then we are finished. Return the minimum length substring found.
+        # s = "ADOBECODEBANC", t = "ABC"
+        l, r = 0, 0
+        end = len(s)-1
+        count_t = Counter(t)
+        count_s = defaultdict(int)
+        smallest_substring = ""
 
-        def equalCount(countS, countT):
-            for char in countT:
-                if countS[char] < countT[char]:
-                    return False
-            return True
+        while r <= end:
+            char = s[r]
+            count_s[char] += 1
+            # invalid window
 
-        for r in range(n):
-            countS[s[r]] += 1
-            # valid, but we want to look for the min window so shrink
-            while equalCount(countS, countT):
-                # update bestMatch
-                if bestMatch == "" or (r - l + 1) < len(bestMatch):
-                    bestMatch = s[l:r+1]
+            # valid window
+            while all(count_s[k] >= v for k,v in count_t.items()):
+                if smallest_substring == "" or r-l+1 < len(smallest_substring):
+                    smallest_substring = s[l:r+1]
 
-                countS[s[l]] -= 1
-                l += 1
-        return bestMatch
+                char_remove = s[l]
+                count_s[char_remove] -=1
+                if count_s[char_remove] == 0:
+                    del count_s[char_remove]
+                l+=1
+            
+            r+=1
 
-        
+        return smallest_substring
+                
